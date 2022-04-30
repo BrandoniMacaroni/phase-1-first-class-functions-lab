74b8c9ea924508071ea9941ab1c348a854288859
Brandon <bcharris713@gmail.com>
2022-04-30 00:49:52 -0500

    commit tree 65cfd4e8a361c1cadf0d59366ba382b60e371dff

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(list) {
+    return list.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(list) {
+    return list.slice(-2);
+}
+
+var selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+const createFareMultiplier = function(numberOfTickets){
+    return function(fare) {
+        return  numberOfTickets * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, thing) {
+    if (thing === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    } else {
+        return returnLastTwoDrivers(drivers);
+    }
+}
